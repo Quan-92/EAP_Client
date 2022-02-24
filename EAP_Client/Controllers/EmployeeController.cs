5fef1ae2073379f5c2a9eb532e6f39d3caf31ddf
TranQuan <9602tranquan@gmail.com>
2022-02-24 14:50:40 +0700

    TranQuan

---
+++ EAP_Client/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using EAP_Client.EAP_Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EAP_Client.Controllers
+{
+    public class EmployeeController : Controller
+    {
+   
+        static EAP_Client.Service1Client service1Client = new EAP_Client.Service1Client();
+        public ActionResult Index()
+        {
+            var listEmployee = service1Client.GetList();
+            return View(listEmployee);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(Employee employee)
+        {
+            service1Client.Save(employee);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
